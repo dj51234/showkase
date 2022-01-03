a66a6b8b22c8d821269c3919262ed3b161f07ee1
dj51234 <dj51234@yahoo.com>
2022-01-03 18:41:47 -0500

    added pages/styles for login/register/dashboard

--- showkase_app/urls.py
+++ showkase_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'showkase'
 
 urlpatterns = [
-    path('home/',views.render_home,name="render_home")
+    path('home/',views.render_home,name="render_home"),
+    path('login/',views.render_login,name="render_login"),
+    path('register/',views.render_register,name="render_register"),
+    path('dashboard/',views.render_dashboard,name="render_dashboard")
 ]
--- showkase_app/views.py
+++ showkase_app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def render_home(request):
     return render(request, 'showkase_app/index.html')
+
+def render_register(request):
+    return render(request, 'showkase_app/register.html')
+
+def render_login(request):
+    return render(request, 'showkase_app/login.html')
+
+def render_dashboard(request):
+    return render(request, 'showkase_app/dashboard.html')
