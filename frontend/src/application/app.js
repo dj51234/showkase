153428c752d15c25afb6fa8248eded6dfd8e3151
Derrick Johnson <10237736+dj51234@users.noreply.github.com>
2022-01-10 04:10:37 -0500

    javascript start for creating result elements

--- frontend/src/application/app.js
+++ frontend/src/application/app.js
@@ -8,6 +8,20 @@ import "bootstrap/dist/js/bootstrap.bundle";
 // We can import other JS file as we like
 import "../components/sidebar";
 
+const results = document.querySelector(".results");
+
 window.document.addEventListener("DOMContentLoaded", function () {
   window.console.log("dom ready 1");
+  // const div = document.createElement("div");
+  // div.classList.add("result");
+  // div.textContent = "this is name";
 });
+
+let div = document.createElement("div");
+div.classList.add("result");
+let p = document.createElement("p");
+p.classList.add("result-name");
+p.classList.add("text-sm");
+p.textContent = "The name";
+div.appendChild(p);
+results.insertAdjacentElement("afterbegin", div);
